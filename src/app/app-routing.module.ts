7069a3bf3eb4684a3662bb904c448b849a552ba4
Mateusz <mateusz.cabala95@gmail.com>
2020-04-25 20:56:41 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
+import {NewsComponent} from "./pages/news/news.component";
+import {ContactComponent} from "./pages/contact/contact.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'about-us', component:AboutUsComponent},
+  {path:'news', component:NewsComponent, children:[
+      {path:'news/:id', component:NewsComponent},
+    ]},
+  {path:'contact', component:ContactComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

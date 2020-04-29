3125c07046997f910e681067f2f96e6c57f3ef16
Mateusz <mateusz.cabala95@gmail.com>
2020-04-29 20:40:28 +0200

    Create part of Home page
    Add data in section rorun

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FeatureModel} from "../../models/Feature.model";
+import {HomePageFeatures} from "./mock.data";
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  features:Array<FeatureModel> = HomePageFeatures;
+
 
   constructor() { }
 
---
+++ src/app/pages/home/mock.data.ts
@@ -0,0 +1,17 @@
+export const HomePageFeatures = [
+  {
+    image: 'assets/home/icon_1.jpg',
+    title: 'Cotton candy',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur autem, cumque dignissimos, ducimus eaque, et fugit incidunt labore laudantium obcaecati quas quis totam unde voluptates?',
+  },
+  {
+    image: 'assets/home/icon_2.jpg',
+    title: 'Tootsie roll cotton',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur autem, cumque dignissimos, ducimus eaque, et fugit incidunt labore laudantium obcaecati quas quis totam unde voluptates?',
+  },
+  {
+    image: 'assets/home/icon_3.jpg',
+    title: 'A little bit of magic',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur autem, cumque dignissimos, ducimus eaque, et fugit incidunt labore laudantium obcaecati quas quis totam unde voluptates?',
+  }
+];

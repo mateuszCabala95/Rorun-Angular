be10ab814e4649a956502816422e569986a7cb34
Mateusz <mateusz.cabala95@gmail.com>
2020-04-30 21:14:14 +0200

    Finish PhotoCard

--- src/app/components/photo-card/photo-card.component.ts
+++ src/app/components/photo-card/photo-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-photo-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-card.component.scss']
 })
 export class PhotoCardComponent implements OnInit {
+
+  @Input() photo;
+  @Input() name;
+  @Input() subtitle;
+
 
   constructor() { }
 
